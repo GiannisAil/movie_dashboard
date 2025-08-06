e29f553ac03762b8eb646ff626f55b7b452151d6
mosla <giannisailamakis1@gmail.com>
2025-08-06 12:58:39 +0300

    testing post method

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None 
+    price: float
+    tax: float | None = None
 
 app = FastAPI()
 
-@app.get("/api/health")
-def health_check():
-    return {"status": "ok"}
+@app.get("/")
+async def root():
+    return {"message": "Hello World!"}
+
+@app.get("/movie/{movieid}")
+def test(movieid: int):
+    return {"received": movieid}
+
+@app.get("/files/{filepath:path}")
+async def read_file(filepath: str):
+    return {"filepath": filepath}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
