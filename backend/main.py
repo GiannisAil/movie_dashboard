24c39b3e330bf2a23edc757c64d64f8be27697d3
mosla <giannisailamakis1@gmail.com>
2025-08-06 16:56:23 +0300

    testing data validation

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Annotated
 from pydantic import BaseModel
 
 class Item(BaseModel):
@@ -24,3 +25,11 @@ async def read_file(filepath: str):
 @app.post("/items/")
 async def create_item(item: Item):
     return item
+
+# query data validation (using Annotated & Query)
+@app.get("/itemsval/")
+async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
+    results = {"items": [{"item_id":"foo"}, {"item_id": "bar"}]}
+    if q:
+        results.update({"q":q})
+    return results
