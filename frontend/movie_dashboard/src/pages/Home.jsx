3344fca0263dcf0339499870696777e1409478f4
mosla <giannisailamakis1@gmail.com>
2025-08-08 14:46:34 +0300

    Added basic frontned

---
+++ frontend/movie_dashboard/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home(){
+    return (
+        <div>
+            <h1>Movie Dashboard</h1>
+            <h4>Movie List.</h4>
+            <h4>Add a movie</h4>
+        </div>
+    );
+}
+
+export default Home;
